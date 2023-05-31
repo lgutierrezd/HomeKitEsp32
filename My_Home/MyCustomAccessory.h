b2371917f28e74df90bcbd261e944f1f0ab75666
lgutierrezd <luchogd26@hotmail.com>
2023-05-31 15:37:01 -0600

    First commit

---
+++ My_Home/MyCustomAccessory.h
@@ -0,0 +1,16 @@
+// class MyCustomAccessory : public HomeSpanAccessory {
+// public:bool handleAuthentication(const std::string& setupCode) {
+//   // Aquí es donde debes implementar tu lógica de autenticación personalizada.
+//   // Compara el código de configuración recibido con el código de configuración esperado.
+//   // Si coinciden, devuelve true; de lo contrario, devuelve false.
+
+//     std::string expectedSetupCode = "123-45-678"; // Código de configuración esperado
+
+//     if (setupCode == expectedSetupCode) {
+//       return true; // Código de configuración válido
+//     } else {
+//       return false; // Código de configuración inválido
+//     }
+//   }  
+
+// };
